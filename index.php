e636f40b37d5262fe774924ce897818e9e3857eb
GoharAliJafri <gohar.jaff2512@gmail.com>
2025-04-12 03:11:16 +0500

    Logo changes and more

---
+++ index.php
@@ -0,0 +1,270 @@
+<?php
+include('header.php');
+?>
+		
+		<!-- Slider Area -->
+		<section class="slider">
+			<div class="hero-slider">
+				<!-- Start Single Slider -->
+				<div class="single-slider" style="background-image:url('img/slider2.png')">
+					<div class="container">
+						<div class="row">
+							<div class="col-lg-7">
+								<div class="text">
+									<h1>Elevate your business effortlessly!</h1>
+									<p>Revolutionize your schedule with our expert Appointment/DM Setters, ready to secure 2-4 appointments daily for you.</p>
+									<div class="button">
+										<a href="#" class="btn">Schedule A Demo Call</a>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<!-- End Single Slider -->
+				<!-- Start Single Slider -->
+				<div class="single-slider" style="background-image:url('img/slider.jpg')">
+					<div class="container">
+						<div class="row">
+							<div class="col-lg-7">
+								<div class="text">
+									<h1>We Provide <span>Medical</span> Services That You Can <span>Trust!</span></h1>
+									<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris sed nisl pellentesque, faucibus libero eu, gravida quam. </p>
+									<div class="button">
+										<a href="#" class="btn">Get Appointment</a>
+										<a href="#" class="btn primary">About Us</a>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<!-- Start End Slider -->
+				<!-- Start Single Slider -->
+				<!-- <div class="single-slider" style="background-image:url('img/slider3.png')">
+					<div class="container">
+						<div class="row">
+							<div class="col-lg-7">
+								<div class="text">
+									<h1>We Provide <span>Medical</span> Services That You Can <span>Trust!</span></h1>
+									<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris sed nisl pellentesque, faucibus libero eu, gravida quam. </p>
+									<div class="button">
+										<a href="#" class="btn">Get Appointment</a>
+										<a href="#" class="btn primary">Conatct Now</a>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div> -->
+				<!-- End Single Slider -->
+			</div>
+		</section>
+		<!--/ End Slider Area -->
+		
+		<!-- Start Schedule Area -->
+		<section class="schedule" style="background-color: #17202c !important;">
+			<div class="container">
+				<div class="schedule-inner">
+					<div class="row">
+						<div class="col-lg-4 col-md-6 col-12 " style="background-color: #313949 !important;">
+							<!-- single-schedule -->
+							<div class="single-schedule first" style="background-color: #4b4f58 !important;">
+								<div class="inner">
+									<div class="icon">
+										<i class="fa fa-ambulance"></i>
+									</div>
+									<div class="single-content">
+										<!-- <span>Lorem Amet</span> -->
+										<h4>High-Ticket Sales System</h4>
+										<p>Sales Scripts, Objection Scripts, Email Follow Ups And More!</p>
+										<a href="#">LEARN MORE<i class="fa fa-long-arrow-right"></i></a>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="col-lg-4 col-md-6 col-12" style="background-color: #313949 !important;">
+							<!-- single-schedule -->
+							<div class="single-schedule middle" style="background-color: #4b4f58 !important;">
+								<div class="inner">
+									<div class="icon">
+										<i class="icofont-prescription"></i>
+									</div>
+									<div class="single-content">
+										<!-- <span>Fusce Porttitor</span> -->
+										<h4>Lead Generation Systems</h4>
+										<p>Organic Outbound Appointment Setting Strategy, Messaging And More!</p>
+										<a href="#">LEARN MORE<i class="fa fa-long-arrow-right"></i></a>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="col-lg-4 col-md-12 col-12" style="background-color: #313949 !important;">
+							<!-- single-schedule -->
+							<div class="single-schedule last" style="background-color: #4b4f58 !important;">
+								<div class="inner">
+									<div class="icon">
+										<i class="icofont-ui-clock"></i>
+									</div>
+									<div class="single-content">
+										<!-- <span>Donec luctus</span> -->
+										<h4>Team Building</h4>
+										<p>Certified Appointment Setters & Placement - Hiring, Managing & Tracking Systems</p>
+										<a href="#">LEARN MORE<i class="fa fa-long-arrow-right"></i></a>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+		<!--/End Start schedule Area -->
+
+		<!-- Start Feautes -->
+		<section class="Feautes section" style="background-color: #17202c !important;">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-12">
+						<div class="">
+							<h1 style="color: #fff;">Appointments Has Never Been This Easy</h1>
+							<img src="img/section-img.png" alt="#">
+							<ul class="mt-3 ps-0" style="list-style: none; color: #fff;">
+								<li><i class="fa fa-check-circle text-warning me-2"></i>&nbsp;&nbsp;&nbsp;Prospecting on different social media profiles (Facebook, LinkedIn, Instagram)</li>
+								<li><i class="fa fa-check-circle text-warning me-2"></i>&nbsp;&nbsp;&nbsp;Email marketing</li>
+								<li><i class="fa fa-check-circle text-warning me-2"></i>&nbsp;&nbsp;&nbsp;Manages a large volume of queries from your customers each day</li>
+								<li><i class="fa fa-check-circle text-warning me-2"></i>&nbsp;&nbsp;&nbsp;Promotes your services to the target audience</li>
+								<li><i class="fa fa-check-circle text-warning me-2"></i>&nbsp;&nbsp;&nbsp;Study the clientele to locate, market, and sell to other customers</li>
+								<li><i class="fa fa-check-circle text-warning me-2"></i>&nbsp;&nbsp;&nbsp;Makes product or service information freely accessible across a range of platforms</li>
+								<li><i class="fa fa-check-circle text-warning me-2"></i>&nbsp;&nbsp;&nbsp;Answers the client’s queries</li>
+								<li><i class="fa fa-check-circle text-warning me-2"></i>&nbsp;&nbsp;&nbsp;Find your ideal client and schedule appointments and meetings</li>
+							</ul>
+						</div>
+					</div>
+				</div>								
+			</div>
+		</section>
+		<!--/ End Feautes -->
+		
+		<!-- Start Blog Area -->
+		<section class="blog section" id="blog" style="background-color: #17202c !important;">
+			<div class="container-fluid">
+				<div class="row">
+					<div class="col-lg-12" style="background-color: #f9c349; width: 100%;">
+						<div class="section-title px-0">
+							<h1 style="color: #fff; font-size: 90px;">Take your Business To Next Level</h1>
+							<a href="#contact" class="btn btn-primary mt-4"
+								style="padding: 12px 30px; font-size: 18px; border-radius: 30px; background: linear-gradient(135deg, #00c6ff, #0072ff); border: none;">
+								<i class="fa fa-calendar-check me-2"></i>&nbsp;&nbsp;Schedule a Demo Call
+							</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+		<!-- End Blog Area -->
+		<section class="py-5" style="background-color: #17202c !important;">
+			<div class="container">
+				<!-- Row 1 -->
+				<div class="row align-items-center">
+					<div class="col-md-8" style="background-color: #4d4a46 !important;">
+						<h6 style="color: #fff;" class="py-3">-Connor Trott</h6>
+						<p style="color: #fff;font-size: 40px; line-height: 60px; font-weight: 600;">Booking 3-6 calls per day is an absolute game changer for me</p>
+					</div>
+					<div class="col-md-4 text-center">
+						<img src="img/slider.jpg" class="img-fluid" alt="Grow Audience">
+					</div>
+				</div>
+				<hr>
+				<!-- Row 2 -->
+				<div class="row align-items-center mb-5">
+					<div class="col-md-4 text-center">
+						<img src="img/slider.jpg" class="img-fluid" alt="Workflow Automation">
+					</div>
+					<div class="col-md-8" style="background-color: #4d4a46 !important;">
+						<h6 style="color: #fff;" class="py-3">Results-Oriented Solutions</h6>
+						<p style="color: #fff;font-size: 40px; line-height: 60px; font-weight: 600;">I really see the results of my Coaching business within first 12 days</p>
+					</div>
+				</div>
+				<hr>
+				<!-- Row 3 -->
+				<div class="row align-items-center">
+					<div class="col-md-8" style="background-color: #4d4a46 !important;">
+						<h6 style="color: #fff;" class="py-3">From Art to Science</h6>
+						<p style="color: #fff;font-size: 40px; line-height: 60px; font-weight: 600;">We are currently booking 8-13 calls per day</p>
+					</div>
+					<div class="col-md-4 text-center">
+						<img src="img/slider.jpg" class="img-fluid" alt="Sales Boost">
+					</div>
+				</div>
+			</div>
+		</section>
+
+		<!-- Start case study -->
+		<section class="blog section" id="blog" style="background-color: #17202c !important;">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-12">
+						<div class="section-title">
+							<h1 style="color: #fff;">Case Studies</h1>
+							<!-- <img src="img/section-img.png" alt="#">
+							<p>Lorem ipsum dolor sit amet consectetur adipiscing elit praesent aliquet. pretiumts</p> -->
+						</div>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-lg-6">
+						<img src="img/slider.jpg" style="max-width: 106%;" alt="#">
+					</div>
+					<div class="col-lg-6">
+						<img src="img/slider2.png" style="max-width: 106%;" alt="#">
+					</div>
+				</div>
+			</div>
+		</section>
+		<!-- End case study -->
+		<!-- Start Here is the feedback from our clients -->
+		<section class="blog section" id="blog" style="background-color: #17202c !important;">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-12">
+						<div class="section-title">
+							<h1 style="color: #fff;">Here is the feedback from our clients</h1>
+							<!-- <img src="img/section-img.png" alt="#">
+							<p>Lorem ipsum dolor sit amet consectetur adipiscing elit praesent aliquet. pretiumts</p> -->
+						</div>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-lg-4">
+						<img src="img/slider.jpg" style="max-width: 106%;" alt="#">
+					</div>
+					<div class="col-lg-4">
+						<img src="img/slider2.png" style="max-width: 106%;" alt="#">
+					</div>
+					<div class="col-lg-4">
+						<img src="img/slider.jpg" style="max-width: 106%;" alt="#">
+					</div>
+				</div>
+			</div>
+		</section>
+		<!-- End Here is the feedback from our clients -->
+		<!-- Start Here is the feedback from our clients -->
+		<section class="blog section" id="blog" style="background-color: #17202c !important;">
+			<div class="container-fluid">
+				<div class="row">
+					<div class="col-lg-12" style="background-color: #f9c349; width: 100%;">
+						<div class="section-title px-0">
+							<h1 style="color: #fff; font-size: 90px;">Take your Business To Next Level</h1>
+							<a href="#contact" class="btn btn-primary mt-4"
+								style="padding: 12px 30px; font-size: 18px; border-radius: 30px; background: linear-gradient(135deg, #00c6ff, #0072ff); border: none;">
+								<i class="fa fa-calendar-check me-2"></i>&nbsp;&nbsp;Schedule a Demo Call
+							</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+		<!-- End Here is the feedback from our clients --></div>
+		
+<?php include('footer.php'); ?>
